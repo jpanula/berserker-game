d16894e5cb579db27bf5838a61d0772244247888
Jaakko Panula <panula.jaakko@gmail.com>
2022-05-23 18:09:36 +0300

    implemented health component

---
+++ Assets/Interfaces/IHealth.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Interfaces
+{
+    public interface IHealth
+    {
+        int CurrentHealth { get; }
+        int MaxHealth { get; }
+        bool IsInvulnerable { get; set; }
+        void IncreaseHealth(int amount);
+        
+        /// <summary>
+        /// Decreases health.
+        /// </summary>
+        /// <param name="amount">Amount of health to decrease.</param>
+        /// <returns>True if health decreases to 0 or below, false otherwise.</returns>
+        bool DecreaseHealth(int amount);
+    }
+}
+
